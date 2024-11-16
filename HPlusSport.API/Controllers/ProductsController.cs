8ecfacd421d7057d8b929bb88c71fa52908d83c4
Sergei Gorgun <sergei_gorgun@epam.com>
2024-11-16 17:16:59 +0200

    Refactor GetAllProducts to return ActionResult
    Changed the return type of GetAllProducts in ProductsController from IEnumerable<Product> to ActionResult. Updated the method to return an Ok result containing the array of products, aligning with standard ASP.NET Core practices for HTTP 200 responses.

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -17,9 +17,9 @@ namespace HPlusSport.API.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Product> GetAllProducts()
+        public ActionResult GetAllProducts()
         {
-            return _context.Products.ToArray();
+            return Ok(_context.Products.ToArray());
         }
     }
 }
