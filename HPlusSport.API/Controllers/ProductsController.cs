ba585512daa5de120279ccd4b97d365357b87bb8
Sergei Gorgun <sergei_gorgun@epam.com>
2024-11-16 16:23:52 +0200

    Add project files.

---
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            // Get all products
+            return "All products";
+        }
+    }
+}
---
+++ HPlusSport.API/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace HPlusSport.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual LinkedList<Product> Products { get; set; }
+    }
+}
